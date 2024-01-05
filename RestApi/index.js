6bf236331b59c18e85b5d10b573bbf4c181c3f54
sakin islam <sakin islam79@gmail.com>
2024-01-05 23:31:55 +0600

    rest api project

---
+++ RestApi/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const users = require("./MOCK_DATA.json");
+const app = express();
+
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find((user) => user.id === id);
+  return res.json(user);
+});
+
+app.get("/users", (req, res) => {
+  const html = `<ul>
+   ${users.map((users) => `<li> ${users.first_name}</li>`).join("")}
+  
+  </ul>`;
+  res.send(html);
+});
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
