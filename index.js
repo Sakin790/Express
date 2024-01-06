87a63868de36d9f74c753a1b437f174d314b97ce
sakin islam <sakin islam79@gmail.com>
2024-01-06 10:32:54 +0600

    function add

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function addNum(a, b) {
-    return a+b
+  return a + b;
 }
 
 for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+  const element = array[index];
 }
+
+const myfun = async (x, y) => {
+  return (await x) + y;
+};
+console.log(myfun(8, 8));
