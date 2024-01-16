79dd64c6f7dc1f38fa401278c812b4417ecd4fbb
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2024-01-16 23:22:17 +0600

    Create Cluster-Load-Balance.js

---
+++ Cluster-Load-Balance.js
@@ -0,0 +1,23 @@
+const cluster = require("cluster");
+const os = require("os");
+const numCPUs = os.cpus().length;
+
+if (cluster.isPrimary) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  const express = require("express");
+  const app = express();
+  const PORT = 8000;
+
+  app.get("/", (req, res) => {
+    return res.json({
+      message: `This is Home Route ${process.pid}`,
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+  });
+}
