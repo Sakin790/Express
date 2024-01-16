f07ac9a3201d43b98e5da768335cdbcf91c604c8
sakin_dev@79 <107269051+Sakin790@users.noreply.github.com>
2024-01-16 23:39:15 +0600

    comment on Cluster-Load-Balance.js

--- Cluster-Load-Balance.js
+++ Cluster-Load-Balance.js
@@ -2,6 +2,7 @@ const cluster = require("cluster");
 const os = require("os");
 const numCPUs = os.cpus().length;
 
+//Amar machine a joto core ace toto gulo load Balancer create kora  jabe
 if (cluster.isPrimary) {
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
